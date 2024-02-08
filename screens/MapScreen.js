8e08b7de2bdce26bf0575aa7c5e093ede47dd149
ulasyildiz <ulasyildiz@sabanciuniv.edu>
2024-02-08 21:04:16 +0300

    Renk Paletleri güncellendi ve figma tasarımlarına uygun hale getirildi

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,66 +1,81 @@
 import React from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, SafeAreaView, Platform, StatusBar } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
 import ways from '../data/ways.json';
-import colors from '../styles/colors.js';
+import colors from '../styles/colors.js'; // Renklerin yolu doğru sağlandığından emin olun
 
 const MapScreen = () => {
-    return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 40.89360763590254, // Güncellenmiş kampüs enlem koordinatı
-            longitude: 29.380166148372258, // Güncellenmiş kampüs boylam koordinatı
-            latitudeDelta: 0.01, // Uygun zoom seviyesi için ayarlanmış delta değeri
-            longitudeDelta: 0.01, // Uygun zoom seviyesi için ayarlanmış delta değeri
-          }}
-        >
-          {ways.map((way, index) => (
-            <Polyline
-              key={index}
-              coordinates={way.coords.map(({ lat, lon }) => ({
-                latitude: parseFloat(lat),
-                longitude: parseFloat(lon),
-              }))}
-              strokeColor={colors.primary.lightBlue1} // Yolun rengi için ana renkleri kullan
-              strokeWidth={3} // Yolun kalınlığı
-            />
-          ))}
-        </MapView>
-        {/* Arama çubuğu ekleyelim */}
-        <TextInput
-          style={styles.searchBar}
-          placeholder="Try “FENS” or “Akbank” etc."
-          placeholderTextColor={colors.blackAndWhite.mediumGrey}
-        />
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Kampüs Haritası</Text>
       </View>
-    );
-  };
-  
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 40.89360763590254,
+          longitude: 29.380166148372258,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        {ways.map((way, index) => (
+          <Polyline
+            key={index}
+            coordinates={way.coords.map(({ lat, lon }) => ({
+              latitude: parseFloat(lat),
+              longitude: parseFloat(lon),
+            }))}
+            strokeColor={colors.primary.mediumBlue} // Polyline rengini mediumBlue olarak güncelle
+            strokeWidth={3}
+          />
+        ))}
+      </MapView>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Try “FENS” or “Akbank” etc."
+        placeholderTextColor={colors.blackAndWhite.mediumGrey}
+      />
+    </SafeAreaView>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.primary.darkBlue, // Header ve SafeAreaView arka plan rengi
+  },
+  header: {
+    height: 60,
+    backgroundColor: colors.primary.darkBlue,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  headerText: {
+    color: colors.blackAndWhite.lightestGrey,
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
   searchBar: {
     position: 'absolute',
-    top: 40,
-    left: 10,
-    right: 10,
-    backgroundColor: colors.blackAndWhite.lightestGrey,
-    borderRadius: 20,
+    top: 70,
+    left: 20,
+    right: 20,
+    backgroundColor: colors.blackAndWhite.lightestGrey, // Arama çubuğu arka plan rengini lightestGrey olarak güncelle
+    borderRadius: 25,
     fontSize: 15,
     padding: 15,
-    elevation: 5, // Sadece Android için gölge efekti
-    shadowColor: colors.blackAndWhite.black, // Sadece iOS için gölge efekti
-    shadowOffset: { width: 0, height: 2 }, // Sadece iOS için gölge efekti
-    shadowOpacity: 0.1, // Sadece iOS için gölge efekti
-    shadowRadius: 5, // Sadece iOS için gölge efekti
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
   },
-  // Diğer stiller...
+  // Diğer stil tanımları...
 });
 
 export default MapScreen;
--- styles/colors.js
+++ styles/colors.js
@@ -1,27 +1,32 @@
 // colors.js
 const colors = {
-    primary: {
-      darkBlue: '#013369',
-      mediumBlue: '#418FDE',
-      lightBlue1: '#8BBEE8',
-      lightBlue2: '#B8D8EB',
-    },
-    secondary: {
-      darkCyan: '#005E52',
-      yellowGreen: '#F1BE48',
-      yellow: '#FCD757',
-      pink: '#EF5B5B',
-    },
-    blackAndWhite: {
-      black: '#101820',
-      darkGrey: '#5B5D63',
-      mediumGrey: '#878B8F',
-      lightGrey1: '#B0B3B5',
-      lightGrey2: '#CFD1D2',
-      lighterGrey: '#E7E8E9',
-      lightestGrey: '#F3F3F4',
-    },
-  };
-  
-  export default colors;
-  
+  primary: {
+    darkBlue: '#013369',
+    mediumBlue: '#418FDE',
+    lightBlue1: '#8BBEE8',
+    lightBlue2: '#B8D8EB',
+    lightGrey: '#BBBBBB',  // Pantone 277 renk tonunu temsil eder
+  },
+  secondary: {
+    cyan: '#00B5E2', // Pantone 306 C, cyan rengi
+    turquoise: '#47D7AC', // Pantone 3385 C, açık yeşil rengi
+    green: '#EF5B5B', // Pantone x, yeşil rengi
+    yellow: '#F1BE48', // Pantone 142 C, sarı rengi
+    lightYellow: '#FCD757', // Pantone 121 C, açık sarı rengi
+    coral: '#EF5B5B', // Bu, Pantone x'in verdiği renk, benzer bir isim verildi
+  },
+  blackAndWhite: {
+    black: '#101820',
+    darkGrey: '#5B5D63',
+    mediumGrey: '#878B8F',
+    lightGrey1: '#B0B3B5',
+    lightGrey2: '#CFD1D2',
+    lighterGrey: '#E7E8E9',
+    lightestGrey: '#F3F3F4',
+    // Pantone Black C %'leri eklenebilir, örnek olarak %70 ve %33 eklendi
+    black70: '#585D63', // Pantone Black C 70%
+    black33: '#B0B3B5', // Pantone Black C 33%
+  },
+};
+
+export default colors;
