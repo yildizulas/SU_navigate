6421f8191f4182bbde1e39d4742e6a55be19699d
ulasyildiz <ulasyildiz@sabanciuniv.edu>
2024-05-30 21:08:21 +0300

    main

--- Api/main.js
+++ Api/main.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-const BASE_URL = 'http://192.168.94.50:8000'; //Platform.OS === 'ios' ? 'http://127.0.0.1:8000' : 'http://10.0.2.2:8000';
+const BASE_URL = Platform.OS === 'ios' ? 'http://127.0.0.1:8000' : 'http://10.0.2.2:8000';
 
 
 // Fetch all faculty members
