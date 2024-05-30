69722cbdc10ce0601448521a4bda4a370bc71af6
ulasyildiz <ulasyildiz@sabanciuniv.edu>
2024-05-30 22:59:14 +0300

    oda içi okay

--- screens/FloorPlanScreenSVG.js
+++ screens/FloorPlanScreenSVG.js
@@ -22,17 +22,52 @@ const FloorPlanScreenSVG = () => {
     let content = await FileSystem.readAsStringAsync(asset.localUri);
 
     const roomElements = `
-      <text x="160" y="225" font-family="Arial" font-size="15" fill="blue" id="room1040" cursor="pointer">1040</text>
-      <text x="150" y="265" font-family="Arial" font-size="15" fill="blue" id="room1039" cursor="pointer">1039</text>
-      <text x="150" y="295" font-family="Arial" font-size="15" fill="blue" id="room1038" cursor="pointer">1038</text>
-      <text x="150" y="330" font-family="Arial" font-size="15" fill="blue" id="room1037" cursor="pointer">1037</text>
-      <text x="324" y="627" font-family="Arial" font-size="15" fill="blue" id="room1022" cursor="pointer">1022</text>
-      <text x="152" y="743" font-family="Arial" font-size="15" fill="blue" id="room1014" cursor="pointer">1014</text>
-      <text x="628" y="152" font-family="Arial" font-size="15" fill="blue" id="room1064" cursor="pointer">1064</text>
-      <text x="768" y="590" font-family="Arial" font-size="15" fill="blue" id="room1100" cursor="pointer">1100</text>
-      <text x="663" y="406" font-family="Arial" font-size="15" fill="blue" id="room1089" cursor="pointer">1089</text>
-      <text x="835" y="818" font-family="Arial" font-size="15" fill="blue" id="room1110" cursor="pointer">1110</text>
+      <g id="room1040" cursor="pointer">
+        <rect x="159" y="210" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="160" y="225" font-family="Arial" font-size="15" fill="blue">1040</text>
+      </g>
+      <g id="room1039" cursor="pointer">
+        <rect x="149" y="250" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="150" y="265" font-family="Arial" font-size="15" fill="blue">1039</text>
+      </g>
+      <g id="room1038" cursor="pointer">
+        <rect x="149" y="280" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="150" y="295" font-family="Arial" font-size="15" fill="blue">1038</text>
+      </g>
+      <g id="room1037" cursor="pointer">
+        <rect x="149" y="315" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="150" y="330" font-family="Arial" font-size="15" fill="blue">1037</text>
+      </g>
+      <g id="room1022" cursor="pointer">
+        <rect x="323" y="612" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="324" y="627" font-family="Arial" font-size="15" fill="blue">1022</text>
+      </g>
+      <g id="room1014" cursor="pointer">
+        <rect x="151" y="728" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="152" y="743" font-family="Arial" font-size="15" fill="blue">1014</text>
+      </g>
+      <g id="room1064" cursor="pointer">
+        <rect x="624" y="137" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="625" y="152" font-family="Arial" font-size="15" fill="blue">1064</text>
+      </g>
+      <g id="room1100" cursor="pointer">
+        <rect x="767" y="575" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="768" y="590" font-family="Arial" font-size="15" fill="blue">1100</text>
+      </g>
+      <g id="room1089" cursor="pointer">
+        <rect x="662" y="391" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="663" y="406" font-family="Arial" font-size="15" fill="blue">1089</text>
+      </g>
+      <g id="room1110" cursor="pointer">
+        <rect x="834" y="803" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="835" y="818" font-family="Arial" font-size="15" fill="blue">1110</text>
+      </g>
+      <g id="room1091" cursor="pointer">
+        <rect x="662" y="465" width="35" height="20" fill="none" stroke="blue" stroke-width="2"/>
+        <text x="663" y="480" font-family="Arial" font-size="15" fill="blue">1091</text>
+      </g>
     `;
+
     const doorRect = `
       <circle cx="812.56884765625" cy="662.219970703125" r="10" fill="red" id="doorRect"/>
     `;
@@ -116,9 +151,9 @@ const FloorPlanScreenSVG = () => {
   const resetPreviousSelection = () => {
     if (previousRoom) {
       webViewRef.current.injectJavaScript(`
-        document.getElementById('room${previousRoom}').setAttribute('fill', 'blue');
-        var existingMarker = document.getElementById('target-marker');
-        if (existingMarker) existingMarker.remove();
+        var rect = document.getElementById('room${previousRoom}').querySelector('rect');
+        rect.setAttribute('stroke', 'blue');
+        rect.setAttribute('fill', 'none');
       `);
     }
   };
@@ -177,6 +212,7 @@ const FloorPlanScreenSVG = () => {
     addClickListener('room1100');
     addClickListener('room1089');
     addClickListener('room1110');
+    addClickListener('room1091');
 
     window.findClosestPath = function(roomId) {
       console.log('Function: findClosestPath', roomId);
@@ -287,7 +323,9 @@ const FloorPlanScreenSVG = () => {
       })();
     `);
     webViewRef.current.injectJavaScript(`
-      document.getElementById('room${selectedRoom}').setAttribute('fill', 'red');
+      var rect = document.getElementById('room${selectedRoom}').querySelector('rect');
+      rect.setAttribute('stroke', 'red');
+      rect.setAttribute('fill', 'red');
     `);
     setPreviousRoom(selectedRoom);
     setModalVisible(false);
@@ -307,7 +345,7 @@ const FloorPlanScreenSVG = () => {
         injectedJavaScript={injectedJavaScript}
         onMessage={handleMessage}
       />
-      <Modal
+    <Modal
         animationType="slide"
         transparent={true}
         visible={modalVisible}
@@ -315,15 +353,17 @@ const FloorPlanScreenSVG = () => {
           setModalVisible(!modalVisible);
         }}
       >
-        <View style={styles.modalView}>
-          <Text>Room {selectedRoom}</Text>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={handleGo}>
-              <Text style={styles.buttonText}>Go</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={handleBack}>
-              <Text style={styles.buttonText}>Back</Text>
-            </TouchableOpacity>
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+          <View style={styles.modalView}>
+            <Text style={styles.roomText}>Room {selectedRoom}</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.button} onPress={handleGo}>
+                <Text style={styles.buttonText}>Go</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={handleBack}>
+                <Text style={styles.buttonText}>Back</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Modal>
@@ -339,19 +379,24 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   modalView: {
-    margin: 20,
-    backgroundColor: 'white',
     borderRadius: 20,
     padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
+    bottom: 0,
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
     },
     shadowOpacity: 0.25,
-    shadowRadius: 4,
+    shadowRadius: 3.84,
     elevation: 5,
+    width: '90%',
+    height: '20%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    backgroundColor: "#f9f9f9",
   },
   buttonContainer: {
     flexDirection: 'row',
@@ -368,6 +413,11 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  roomText: {
+    fontSize: 18,
+    fontWeight: "bold",
     textAlign: 'center',
   },
 });
